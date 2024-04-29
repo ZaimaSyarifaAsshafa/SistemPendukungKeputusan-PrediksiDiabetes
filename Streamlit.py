fd840b86e9c424d081daefc2539694046ee2e41f
ZaimaSyarifaAsshafa <138386280+ZaimaSyarifaAsshafa@users.noreply.github.com>
2024-04-29 07:35:12 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -45,8 +45,8 @@ diab_diagnosis = ''
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-    if(diab_prediction[0] == 1):
-        diab_diagnosis = 'Pasien terkena Diabetes'
+    # Menampilkan hasil prediksi
+    if diab_prediction[0] == 1:
+        st.success('Pasien terkena Diabetes')
     else:
-        diab_diagnosis = 'Pasien tidak terkena Diabetes'
-st.success(diab_diagnosis)
+        st.success('Pasien tidak terkena Diabetes')
