394c18f6629ac31b4553e24afb28c9debb57af44
ZaimaSyarifaAsshafa <138386280+ZaimaSyarifaAsshafa@users.noreply.github.com>
2024-06-05 21:41:00 +0700

    Update Streamlit.py
    new 05062024

--- Streamlit.py
+++ Streamlit.py
@@ -14,20 +14,11 @@ st.title('Prediksi Penyakit Diabetes')
 # Membagi kolom
 col1, col2 = st.columns(2)
 
-with col1:
-    Pregnancies = st.text_input('Input nilai Pregnancies')
-
 with col2:
     Glucose = st.text_input('Input nilai Glucose')
 
 with col1:
     BloodPressure = st.text_input('Input nilai Blood Pressure')
-
-with col2:
-    SkinThickness = st.text_input('Input nilai Skin Thickness')
-
-with col1:
-    Insulin = st.text_input('Input nilai Insulin')
 
 with col2:
     BMI = st.text_input('Input nilai BMI')
@@ -44,17 +35,14 @@ diab_diagnosis = ''
 # Membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
     # Konversi nilai input menjadi tipe data numerik
-    Pregnancies = float(Pregnancies)
     Glucose = float(Glucose)
     BloodPressure = float(BloodPressure)
-    SkinThickness = float(SkinThickness)
-    Insulin = float(Insulin)
     BMI = float(BMI)
     DiabetesPedigreeFunction = float(DiabetesPedigreeFunction)
     Age = float(Age)
 
     # Prediksi menggunakan model
-    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    diab_prediction = diabetes_model.predict([[Glucose, BloodPressure, BMI, DiabetesPedigreeFunction, Age]])
 
     # Menampilkan hasil prediksi
     if diab_prediction[0] == 1:
